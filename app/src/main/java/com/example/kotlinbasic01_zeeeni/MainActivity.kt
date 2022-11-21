016fcb8ff4d75dd2f2ca9e113d3b6298bed8ddff
Jieun <wldms941114@gmail.com>
2022-11-21 22:55:55 +0900

    -

--- app/src/main/java/com/example/kotlinbasic01_zeeeni/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasic01_zeeeni/MainActivity.kt
@@ -2,13 +2,23 @@ package com.example.kotlinbasic01_zeeeni
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        clickBtn.setOnClickListener {
 
+            // 코드에 메모 남기기 - 주석
+//            Ctrl + / : 커서가 있는 줄을 주석 처리.
+//            이 { } 안 에 있는 코드만, 버튼이 눌렀을 때 실행됨
+
+            Log.d("메인화면", "클릭용 버튼 눌림")
+
+
+        }
 
 
     }
