53657960f32b78a433e657ef0459c7ec9749724b
Jieun <wldms941114@gmail.com>
2022-11-21 23:09:07 +0900

    토스트 띄워보기

--- app/src/main/java/com/example/kotlinbasic01_zeeeni/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasic01_zeeeni/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.kotlinbasic01_zeeeni
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +19,12 @@ class MainActivity : AppCompatActivity() {
             Log.d("메인화면로그", "클릭용 버튼 눌림")
             Log.e("메인화면로그", "에러 관련 로그")
 
+
+        }
+
+        smallBtn.setOnClickListener {
+
+            Toast.makeText(this, "작은 버튼 눌림", Toast.LENGTH_SHORT).show()
 
         }
 
